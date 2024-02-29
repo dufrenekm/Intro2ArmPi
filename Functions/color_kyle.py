a945a7e21b337f9f6056093d665105e73f51c583
Kyle DuFrene <dufrenek@oregonstate.edu>
2024-02-29 09:23:56 -0800

    added additional contour functions

--- Functions/color_kyle.py
+++ Functions/color_kyle.py
@@ -5,6 +5,13 @@ import Camera
 import cv2
 import numpy as np
 from time import sleep
+from LABConfig import color_range
+import math
+import logging
+logging_format = "%(asctime)s: %(message)s"
+logging.basicConfig(format=logging_format, level=logging.INFO,datefmt="%H:%M:%S")
+logging.getLogger().setLevel(logging.DEBUG)
+
 
 class Color_Track:
     def __init__(self) -> None:
@@ -12,6 +19,7 @@ class Color_Track:
         # Start the camera
         self.my_camera = Camera.Camera()
         self.my_camera.camera_open()
+        self.target = ('red',)
         
         pass
     
@@ -70,7 +78,35 @@ class Color_Track:
         
         return black_img
     
+    def getAreaMaxContour(self, contours):
+        contour_area_temp = 0
+        contour_area_max = 0
+        area_max_contour = None
+
+        for c in contours:  # Traverse all contours
+            contour_area_temp = math.fabs(cv2.contourArea(c))  # Calculate the contour area
+            if contour_area_temp > contour_area_max:
+                contour_area_max = contour_area_temp
+                if contour_area_temp > 300:  # Only when the area is greater than 300, the outline of the largest area is effective to filter interference
+                    area_max_contour = c
+
+        return area_max_contour, contour_area_max  # Return the largest contour
     
+    def get_contours(self, frame):
+        # Convert the image to LAB space
+        frame_lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)  
+        
+        area_max = 0
+        areaMaxContour = 0
+        for i in color_range:
+            if i in self.target:
+                detect_color = i
+                frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])  # Perform bit operations on the original image and mask
+                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # Open operation
+                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # Close operation
+                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # Find the contour
+                areaMaxContour, area_max = self.getAreaMaxContour(contours)  # Find the largest contour
+        logging.debug(f"Area max contour: {areaMaxContour}, Area max: {area_max}")
     
     
     
@@ -81,16 +117,21 @@ class Color_Track:
         
         # Start by getting a frame
         frame = self.get_frame()
-        if not frame:
+        if frame is not None:
             return
+        logging.debug("Got a valid frame.")
         # Pre-process with resizing and blur
         frame = self.pre_process_image(frame)
+        # Get contours 
+        self.get_contours(frame)
+        
         
         
         
     
 if __name__ == '__main__':
     ct = Color_Track()
+    ct.target = ('red',)
     for i in range(10):
         ct.run()
         sleep(.1)
