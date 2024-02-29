37fd26d738d107956d694dbb5c94c390190ab4ec
Kyle DuFrene <dufrenek@oregonstate.edu>
2024-02-29 09:37:21 -0800

    added proccess contour

--- Functions/color_kyle.py
+++ Functions/color_kyle.py
@@ -6,6 +6,10 @@ import cv2
 import numpy as np
 from time import sleep
 from LABConfig import color_range
+sys.path.append('/home/pi/ArmPi/CameraCalibration/')
+from CalibrationConfig import square_length
+sys.path.append('/home/pi/ArmPi/ArmIK/')
+from Transform import getROI, getCenter, convertCoordinate
 import math
 import logging
 logging_format = "%(asctime)s: %(message)s"
@@ -20,6 +24,13 @@ class Color_Track:
         self.my_camera = Camera.Camera()
         self.my_camera.camera_open()
         self.target = ('red',)
+        self.size = (640, 480)
+        self.range_rgb = {
+            'red': (0, 0, 255),
+            'blue': (255, 0, 0),
+            'green': (0, 255, 0),
+            'black': (0, 0, 0),
+            'white': (255, 255, 255)}
         
         pass
     
@@ -107,8 +118,30 @@ class Color_Track:
                 contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # Find the contour
                 areaMaxContour, area_max = self.getAreaMaxContour(contours)  # Find the largest contour
         logging.debug(f"Area max contour: {areaMaxContour}, Area max: {area_max}")
-    
-    
+        return areaMaxContour, area_max, detect_color
+        
+        
+    def process_contour(self, areaMaxContour, area_max, color, img):
+        if area_max < 2500:
+            # TODO: Update this
+            return False
+        
+        rect = cv2.minAreaRect(areaMaxContour)
+        box = np.int0(cv2.boxPoints(rect))
+
+        roi = getROI(box) #Get roi area
+        get_roi = True
+
+        img_centerx, img_centery = getCenter(rect, roi, self.size, square_length)  # Get the center coordinates of the wooden block
+        world_x, world_y = convertCoordinate(img_centerx, img_centery, self.size) #Convert to real world coordinates
+        
+        
+        cv2.drawContours(img, [box], -1, self.range_rgb[color], 2)
+        cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.range_rgb[color], 1) #draw center point
+        distance = math.sqrt(pow(world_x - last_x, 2) + pow(world_y - last_y, 2)) #Compare the last coordinates to determine whether to move
+        last_x, last_y = world_x, world_y
+        track = True
     
     
     def run(self):
@@ -116,15 +149,18 @@ class Color_Track:
         """
         
         # Start by getting a frame
-        frame = self.get_frame()
-        if frame is None:
+        orig_frame = self.get_frame()
+        if orig_frame is None:
             return
         # print(frame.shape)
         logging.debug("Got a valid frame.")
         # Pre-process with resizing and blur
-        frame = self.pre_process_image(frame)
+        frame = self.pre_process_image(orig_frame)
         # Get contours 
-        self.get_contours(frame)
+        shape, area, color = self.get_contours(frame)
+        # Process contours
+        self.process_contour(shape, area, color, orig_frame)
+        
         
         
         
